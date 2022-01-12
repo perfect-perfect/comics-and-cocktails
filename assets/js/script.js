7b2537832502ae30497cb43572fa2a3585bf5c99
Michael Monihan <mrmonihan@hotmail.com>
2022-01-12 18:28:59 -0500

    Added call for recipie, measurements, and instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var cocktailBtn = document.querySelector("#comics-and-cocktails");
 var drinkImg = document.querySelector("#drink-img");
 var comicImg = document.querySelector("#comic-img");
+var recipeList = document.querySelector("#recipe-list");
+var drinkName = document.querySelector("#drink-name");
+var measureList = document.querySelector("#measure-list");
+var instruc = document.querySelector("#instructions");
+
 var comicAmount;
 
 // uses JavaScript Classes to build clock
@@ -58,14 +63,84 @@ clockObject.start();
     })
         .then(data => {
         console.log(data);
+        setDrinkName(data);
         setDrinkImage(data);
+        getIngred(data);
+        setInstruc(data);
     })
         .catch((error) => console.error("Error code:", error));
     };
 
+    function setDrinkName(drinkUrl){
+        const drink = drinkUrl.drinks[0];
+        drinkName.textContent = drink.strDrink;
+
+    }
+    function setInstruc(drinkUrl){
+        const drink = drinkUrl.drinks[0];
+        instruc.textContent = drink.strInstructions;
+
+    }
+
     function setDrinkImage(drinkUrl){
         const drink = drinkUrl.drinks[0];
         drinkImg.src = drink.strDrinkThumb;
+
+    }
+
+    function getIngred(drinkUrl){
+
+        const cocktailIngredients = recipeList;
+        const cocktailAmounts = measureList;
+        const drink = drinkUrl.drinks[0];
+
+        const getIngredients = Object.keys(drink)
+        .filter(function (ingredient) {
+            return ingredient.indexOf("strIngredient") == 0;
+        })
+        .reduce(function (ingredients, ingredient) {
+            if (drink[ingredient] != null) {
+                ingredients[ingredient] = drink[ingredient];
+        }
+        return ingredients;
+        }, {});
+
+        const getAmounts = Object.keys(drink)
+        .filter(function (measure) {
+            return measure.indexOf("strMeasure") == 0;
+        })
+        .reduce(function (measures, measure) {
+            if (drink[measure] != null) {
+                measures[measure] = drink[measure];
+        }
+        
+        return measures;
+        }, {});
+
+        for (let key in getAmounts) {
+
+            let amountVal = getAmounts[key];
+        
+            listItem = document.createElement("li");
+    
+            listItem.innerHTML = amountVal;
+            
+            cocktailAmounts.appendChild(listItem);
+        }
+
+    for (let key in getIngredients) {
+
+        let ingred = getIngredients[key];
+        
+        listItem = document.createElement("li");
+
+        listItem.innerHTML = ingred;
+        
+        cocktailIngredients.appendChild(listItem);
+        
+    }
+
+ 
 
     }
 
@@ -96,7 +171,7 @@ clockObject.start();
         
         var randNumGen = Math.floor((Math.random() * comicAmount) + 1);
 
-        console.log(randNumGen);
+        //console.log(randNumGen);
 
         var apiUrl = "https://cors-anywhere.herokuapp.com/https://xkcd.com/" + randNumGen + "/info.0.json";
 
